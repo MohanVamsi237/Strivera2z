5073f22e7a9eaa31f6cd5fa0de85bf020d8d0568
MohanVamsi237 <mohanvamsi237@gmail.com>
2026-06-06 22:18:45 +0530

    Added a new file

---
+++ arrays/medium/leadersInArray.java
@@ -0,0 +1,20 @@
+package medium;
+import java.util.*;
+class Solution {
+    public List<Integer> leaders(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int max = nums[nums.length - 1];
+        list.add(max);
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > max) {
+                max = nums[i];
+                list.add(nums[i]);
+            }
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+}
