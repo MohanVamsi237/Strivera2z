449dbcce23d1a82c94c135afaea5c353fd231711
MohanVamsi237 <mohanvamsi237@gmail.com>
2026-06-08 23:36:55 +0530

    Added new files

---
+++ arrays/hard/leetcode229.java
@@ -0,0 +1,29 @@
+// Majority Element II
+package hard;
+import java.util.*;
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+            int count = 1;
+
+            while (i + 1 < n && nums[i] == nums[i + 1]) {
+                i++;
+                count++;
+            }
+
+            if (count > n / 3) {
+                list.add(nums[i]);
+            }
+
+            i++;
+        }
+
+        return list;
+    }
+}
