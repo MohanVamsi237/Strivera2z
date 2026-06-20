cc9c9626066a49736fd2618d2cb01d6c2730a6c4
MohanVamsi237 <mohanvamsi237@gmail.com>
2026-06-20 22:20:16 +0530

    added a new file

--- linkedList/LLmedium/deleteMiddle.java
+++ linkedList/LLmedium/deleteMiddle.java
@@ -1,9 +1,20 @@
+package linkedList.LLmedium;
+
 class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
 class SolutionLLLLm10 {
